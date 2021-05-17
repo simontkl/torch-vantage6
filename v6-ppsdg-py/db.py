5b99cf27779c2083ae1ee2ccffbb5af1e1a15af5
simontkl <72913950+simontkl@users.noreply.github.com>
2021-05-17 16:02:45 +0200

    Update db.py

--- v6-ppsdg-py/db.py
+++ v6-ppsdg-py/db.py
@@ -1,2 +1,4 @@
 # this will be done as directed by Yu: send entire dataset (better, sample)
 # to each node and via indexing only have the node work with a partition of that data
+
+# will use fully IID partition
