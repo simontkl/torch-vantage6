32b884c8898e316490efafc3d8786b2809862226
simontkl <simon.tokloth@gmail.com>
2021-06-07 11:53:54 +0200

    updated client.py and removed unnecessary comments

--- v6-ppsdg-py/client.py
+++ v6-ppsdg-py/client.py
@@ -58,7 +58,7 @@ organizations = client.get_organizations_in_my_collaboration()
 org_ids = ids = [organization["id"] for organization in organizations]
 
 
-task = client.create_new_task({'method': 'initialize_training', ids})
+task = client.create_new_task({'method': 'initialize_training'}, ids)
 print(task)
 
 
@@ -88,46 +88,3 @@ results = client.get_results(task_id=task.get("id"))
 master_task = client.create_new_task({"master": 1, "method":"master"}, [ids[0]])
 results = client.get_results(task.get("id"))
 print(results)
-
-
-
-
-
-
-
-
-# we can either test a RPC method or the master method (which will trigger the
-# RPC methods also). Lets start by triggering an RPC method and see if that
-# works. Note that we do *not* specify the RPC_ prefix for the method! In this
-# example we assume that both a.csv and b.csv contain a numerical column `age`.
-# average_partial_task = client.create_new_task(
-#     input_={
-#         'method':'average_partial',
-#         'kwargs': {
-#             'column_name': 'age'
-#         }
-#     },
-#     organization_ids=org_ids
-# )
-
-# You can directly obtain the result (we dont have to wait for nodes to
-# complete the tasks)
-# results = client.get_results(average_partial_task.get("id"))
-
-# To trigger the master method you also need to supply the `master`-flag
-# to the input. Also note that we only supply the task to a single organization
-# as we only want to execute the central part of the algorithm once. The master
-# task takes care of the distribution to the other parties.
-# average_task = client.create_new_task(
-#     input_={
-#         'master': 1,
-#         'method': 'master',
-#         'kwargs': {
-#             'column_name': 'age'
-#         }
-#     },
-#     organization_ids=[org_ids[0]]
-# )
-#
-# results = client.get_results(average_task.get("id"))
-# print(results)
