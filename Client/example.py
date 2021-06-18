5c9b1cfe8bca50516ed475958b6823c2b54b01ad
simon <simon.tokloth@gmail.com>
2021-06-18 12:50:38 +0200

    working with remote interpreter and both local_dp

---
+++ Client/example.py
@@ -0,0 +1,14 @@
+from vantage6.tools.mock_client import ClientMockProtocol
+
+
+client = ClientMockProtocol(
+    datasets=["./local/MNIST/processed/training.pt"],
+    module="v6-ppsdg-py"
+)
+
+organizations = client.get_organizations_in_my_collaboration()
+org_ids = ids = [organization["id"] for organization in organizations]
+
+master_task = client.create_new_task({"master": 1, "method": "master"}, [org_ids[0]])
+results = client.get_results(master_task.get("id"))
+print(results)
